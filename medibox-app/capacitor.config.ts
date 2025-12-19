110fd319668ef06e303eba7532d0e16275fca175
lm308981 <matteo.lacheny@etu.univ-cotedazur.fr>
2025-12-19 11:34:17 +0100

    Suppression medibox-app

--- medibox-app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.medibox.app',
-  appName: 'medibox-app',
-  webDir: 'build'
-};
-
-export default config;
